4339cfc62295de9a8affe62f689f9c166bbcbb1c
Cauã Ghiraldi <124325285+Tap234@users.noreply.github.com>
2025-04-28 21:21:04 -0400

    Create INSERTS.sql
    Create INSERTS.sql

---
+++ INSERTS.sql
@@ -0,0 +1,6 @@
+insert into filme (titulo, duracao_minutos, genero) VALUES
+("pirates of silicon valley", 95, "drama"),
+("The social network", 120, "drama"),
+("jobs", 128, "drama"),
+("the imitacion game", 113, "suspense"),
+("snowden", 134, "suspense");
